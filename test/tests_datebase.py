f94e297637a4ef44aa488fd3018ea8d41099bb61
bunkerwallx <39147256+Bunkerwallx@users.noreply.github.com>
2024-07-23 16:25:40 -0600

    Create tests_datebase.py

---
+++ test/tests_datebase.py
@@ -0,0 +1,10 @@
+import unittest
+from src.database import guardar_informacion
+
+class TestDatabase(unittest.TestCase):
+    def test_guardar_informacion(self):
+        # Escribe tus pruebas aquí
+        pass
+
+if __name__ == "__main__":
+    unittest.main()
