788c5d0b8655590be20468d43700f9c6fc9cabe6
bunkerwallx <39147256+Bunkerwallx@users.noreply.github.com>
2024-07-23 16:24:51 -0600

    Create test_scraper.py

---
+++ test/test_scraper.py
@@ -0,0 +1,26 @@
+import unittest
+from src.scraper import obtener_titulo, obtener_subdominios, obtener_elementos, obtener_metadatos, obtener_tecnologias
+
+class TestScraper(unittest.TestCase):
+    def test_obtener_titulo(self):
+        # Escribe tus pruebas aquí
+        pass
+
+    def test_obtener_subdominios(self):
+        # Escribe tus pruebas aquí
+        pass
+
+    def test_obtener_elementos(self):
+        # Escribe tus pruebas aquí
+        pass
+
+    def test_obtener_metadatos(self):
+        # Escribe tus pruebas aquí
+        pass
+
+    def test_obtener_tecnologias(self):
+        # Escribe tus pruebas aquí
+        pass
+
+if __name__ == "__main__":
+    unittest.main()
